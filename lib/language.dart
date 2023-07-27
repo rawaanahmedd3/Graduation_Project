5a9b9f67ab73c8f8d96a1d9e141f03e09976d2d9
rawaanahmedd3 <99561283+rawaanahmedd3@users.noreply.github.com>
2023-07-28 02:36:10 +0300

    Delete lib directory

--- lib/language.dart
+++
@@ -1,8 +0,0 @@
-import 'package:untitled1/changeLanguage.dart';
-
-class Language{
-  String name;
-  bool isChecked;
-  Language(this.name, {this.isChecked = false});
-}
-final List<Language> languages = [Language('English'), Language('عربي')];
